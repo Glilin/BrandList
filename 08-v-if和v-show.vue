8c319a25c7350eba1a59a1d9a2774251d64e86d9
Glilin <1576378853@163.com>
2019-08-20 15:47:36 +0800

    列表数据案例

---
+++ 08-v-if和v-show.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="div">
+        <p>有点过了啊</p>
+        <p>v-if的使用</p>
+        <p v-if='isShow'>我是使用v-if来控制显示和隐藏的</p>
+        <p>v-if的使用</p>
+        <p v-show='isShow'>我是使用v-show来控制显示和隐藏的</p>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            isShow:false
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+
+</style>
+
